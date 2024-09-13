4f27130685ce7cee67075f6c762e324eb92b3b6a
Willian <willianmarcos907@gmail.com>
2024-09-13 14:14:22 -0300

    add scripts

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const closeModalBtn = document.getElementById("close-modal-btn")
 const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
-
+const observationInput = document.getElementById('observation');
 
 let cart = [];
 
@@ -27,20 +27,20 @@ cartModal.addEventListener("click", function (event) {
 
 closeModalBtn.addEventListener("click", function () {
     cartModal.style.display = "none"
-})
+});
 
 
 menu.addEventListener("click", function (event) {
-    // console.log(event.target)
-    let parentButton = event.target.closest(".add-to-cart-btn")
+    let parentButton = event.target.closest(".add-to-cart-btn");
 
     if (parentButton) {
         const name = parentButton.getAttribute("data-name")
         const price = parseFloat(parentButton.getAttribute("data-price"))
-        addToCart(name, price)
+        addToCart(name, price);
+
     }
 
-})
+});
 
 
 // Função para adicionar no carrinho
@@ -70,6 +70,7 @@ function addToCart(name, price) {
 function updateCartModal() {
     cartItemsContainer.innerHTML = "";
     let total = 0;
+
 
     cart.forEach(item => {
         const cartItemElement = document.createElement("div");
@@ -102,7 +103,7 @@ function updateCartModal() {
         currency: "BRL"
     });
 
-    cartCounter.innerHTML = cart.length;
+    cartCounter.innerHTML = total.toFixed(2);
 
 }
 
@@ -146,7 +147,19 @@ addressInput.addEventListener("input", function (event) {
     }
 
 
-})
+});
+
+observationInput.addEventListener("input", function (event) {
+    let inputObs = event.target.value;
+
+    if (inputObs !== "") {
+        observationInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+
+
+});
+
 
 
 // Finalizar pedido
@@ -156,7 +169,7 @@ checkoutBtn.addEventListener("click", function () {
     if (!isOpen) {
 
         Toastify({
-            text: "Ops o restaurante está fechado!",
+            text: "Restaurante está fechado no momento!",
             duration: 3000,
             close: true,
             gravity: "top", // `top` or `bottom`
@@ -171,7 +184,7 @@ checkoutBtn.addEventListener("click", function () {
     }
 
     if (cart.length === 0) return;
-    if (addressInput.value === "") {
+    if (addressInput.value === "" && observationInput.value === "") {
         addressWarn.classList.remove("hidden")
         addressInput.classList.add("border-red-500")
         return;
@@ -179,15 +192,18 @@ checkoutBtn.addEventListener("click", function () {
 
     //Enviar o pedido para api whats
     const cartItems = cart.map((item) => {
+
         return (
-            ` Qtd: (${item.quantity}) ${item.name}  Preço: R$${(item.price * item.quantity).toFixed(2)} |`
+            ` *Qtd:* (${item.quantity}) ${item.name}  *Preço: R$* ${(item.price * item.quantity).toFixed(2)} | \n `
         )
     }).join("")
 
     const message = encodeURIComponent(cartItems)
-    const phone = "8199995555"
+    const phone = "8194239002"
 
-    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+    window.open(`https://wa.me/${phone}?text=${message} *Total:* ${cartTotal.textContent} 
+         *Observação*: ${observationInput.value}
+         *Endereço*: ${addressInput.value} `, "_blank");
 
     cart = [];
     updateCartModal();
@@ -199,7 +215,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 17 && hora < 22;
+    return hora >= 13 && hora < 22;
     //true = restaurante está aberto 
 }
 
